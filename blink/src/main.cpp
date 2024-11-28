bae2f6c293fe9219f3cdc94a1c68a09dbf690c17
DESKTOP-0RVTM4J\KOSTA <aa@aa.aa.aa>
2024-11-28 17:48:40 +0900

    first commit

---
+++ blink/src/main.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+#include <AHT20.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  Serial.println(result);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  //toggle the LED
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(1000);
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
